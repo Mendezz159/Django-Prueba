47bce5b9f677b79830647cf561e6b1982fc95baf
JulianMendoza <111075879+Mendezz159@users.noreply.github.com>
2025-09-02 21:33:52 -0500

    primer commit

---
+++ apps/productos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    activo = models.BooleanField(default=True)
+    link_imagen = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.nombre
---
+++ apps/productos/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Producto
+from .serializers import ProductoSerializer
+
+class ProductoListView(generics.ListAPIView):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
